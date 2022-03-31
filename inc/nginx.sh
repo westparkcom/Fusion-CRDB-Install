253af4216c4e64e0ee5957550bb8fae6a8fb10a3
jpattWPC <jpatten@westparkcom.net>
2022-03-31 12:59:01 -0500

    Fix cert generation issue

--- inc/nginx.sh
+++ inc/nginx.sh
@@ -6,8 +6,10 @@ cp ./nginx/fusionpbx /etc/nginx/sites-available/fusionpbx
 sed -i /etc/nginx/sites-available/fusionpbx -e "s#unix:.*;#unix:/var/run/php/php${php_ver}-fpm.sock;#g"
 
 #self signed certificate
-nginx_cert=/etc/ssl/certs/ssl-cert-snakeoil.pem
-nginx_key=/etc/ssl/private/ssl-cert-snakeoil.key
+snakeoil_cert=/etc/ssl/certs/ssl-cert-snakeoil.pem
+snakeoil_key=/etc/ssl/private/ssl-cert-snakeoil.key
+nginx_cert=/etc/ssl/certs/nginx.crt
+nginx_key=/etc/ssl/private/nginx.key
 
 if [ .${www_cert} != ."" ]; then
 
@@ -18,8 +20,8 @@ if [ .${www_cert} != ."" ]; then
 	chmod 0600 ${nginx_key}
 else
 	make-ssl-cert generate-default-snakeoil --force-overwrite
-	ln -s ${nginx_key} /etc/ssl/private/nginx.key
-	ln -s ${nginx_cert} /etc/ssl/certs/nginx.crt
+	ln -s ${snakeoil_key} ${nginx_key}
+	ln -s ${snakeoil_cert} ${nginx_cert}
 fi
 
 ln -s /etc/nginx/sites-available/fusionpbx /etc/nginx/sites-enabled/fusionpbx
